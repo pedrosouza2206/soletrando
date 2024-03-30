b8c903a9c5cd4a7594aeea85f304d1ef2a147ea0
pedrosouza2206 <163602006+pedrosouza2206@users.noreply.github.com>
2024-03-30 13:36:35 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("SOLETRANDO \n");
+Console.Write("Digite uma palavra: ");
+string palavra = Console.ReadLine()!;
+var caracteres = palavra.ToCharArray();
+string palavraSoletrada = String.Join('-', caracteres).ToUpper();
+
+Console.WriteLine($"\nSoletrando \"{palavra.ToUpper()}\": {palavraSoletrada} \n");
